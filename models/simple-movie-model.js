bc6ab44af1bd866cea25beb08ea50bd02a1efc0e
TanyaRan <tatyanaran@gmail.com>
2016-11-19 23:53:23 +0200

    no eslint errors

--- models/simple-movie-model.js
+++ models/simple-movie-model.js
@@ -1,36 +1,42 @@
 /* globals require module */
 "use strict";
 
-const mongoose = require("mongoose"),
-    Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 let SimpleMovieSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
     imdbId: {
-        type: String,
-        required: true
+        required: true,
+        type: String
+    },
+    name: {
+        required: true,
+        type: String
     }
 });
 
 //  /title/tt0067992/?ref_=adv_li_tt
 function extractImdbIdFromUrl(url) {
-    let index = url.indexOf("/?ref");
+    let index = url.indexOf("?ref");
+
     return url.substring("/title/".length, index);
 }
 
 let SimpleMovie;
-SimpleMovieSchema.statics.getSimpleMovieByNameAndUrl =
-    function(name, url) {
-        let imdbId = extractImdbIdFromUrl(url);
-        return new SimpleMovie({ name, imdbId });
-    };
+
+SimpleMovieSchema.statics.getSimpleMovieByNameAndUrl = (name, url) => {
+    let imdbId = extractImdbIdFromUrl(url);
+
+    return new SimpleMovie({
+        imdbId,
+        name
+    });
+};
 
 SimpleMovieSchema.virtual.imdbUrl = function() {
-    return `http://imdb.com/title/${this.imdbId}/?ref_=adv_li_tt`;
+    return `http://www.imdb.com/title/${this.imdbId}/?ref_=adv_li_tt`;
 };
+
 
 mongoose.model("SimpleMovie", SimpleMovieSchema);
 SimpleMovie = mongoose.model("SimpleMovie");
