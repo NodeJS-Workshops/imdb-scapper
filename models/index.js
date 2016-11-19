18322401ae835c93cc4c38cf6c9e0e80987a7654
TanyaRan <tatyanaran@gmail.com>
2016-11-19 23:49:20 +0200

    no eslint errors

--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 /* globals module require */
+"use strict";
 
 const SimpleMovie = require("./simple-movie-model");
 
 module.exports = {
     getSimpleMovie(name, url) {
-        return SimpleMovie.getSimpleMovieByNameAndUrl(name, url);
+        return SimpleMovie.getSimpleMovieByNAmeAndUrl(name, url);
     },
     insertManySimpleMovies(movies) {
         SimpleMovie.insertMany(movies);
